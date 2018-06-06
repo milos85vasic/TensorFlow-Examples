a6e662f029eb77ee4c6bd0e9e487341194b6fe16
Milos Vasic <milos85vasic@gmail.com>
2018-06-06 15:59:11 +0200

    TensorFlow Examples: Resolve warning.

--- Example_02/example_02.py
+++ Example_02/example_02.py
@@ -1,4 +1,8 @@
+import os
 import tensorflow as tf
+
+# Resolves 'Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX2 FMA' warning:
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 h = tf.constant("Hello")
 w = tf.constant(" World!")
