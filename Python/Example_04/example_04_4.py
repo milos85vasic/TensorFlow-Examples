90d9ad96600e1e02852bbe0137b8bd3b7389ba7e
Milos Vasic <milos85vasic@gmail.com>
2018-06-07 18:30:45 +0200

    TensorFlow Examples: Note added.

--- Python/Example_04/example_04_4.py
+++ Python/Example_04/example_04_4.py
@@ -8,6 +8,8 @@ d = tf.multiply(a, b)
 e = tf.add(c, b)
 f = tf.subtract(d, e)
 
+# NOTE: Opening a session using the with clause will ensure the session is
+# automatically closed once all computations are done.
 with tf.Session() as sess:
     fetches = [a, b, c, d, e, f]
     outs = sess.run(fetches)
