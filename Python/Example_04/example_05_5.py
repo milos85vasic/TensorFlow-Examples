31df9e7f045acf3562a5e4fd0208dce0c61363b6
Milos Vasic <milos85vasic@gmail.com>
2018-06-07 18:42:22 +0200

    TensorFlow Examples: Casting.

--- Python/Example_04/example_05_5.py
+++ Python/Example_04/example_05_5.py
@@ -3,3 +3,8 @@ import tensorflow as tf
 c = tf.constant(4.0, dtype=tf.float64)
 print(c)
 print(c.dtype)
+
+x = tf.constant([1, 2, 3], name='x', dtype=tf.float32)
+print(x.dtype)
+x = tf.cast(x, tf.int64)
+print(x.dtype)
