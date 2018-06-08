20ccdf96488325c72e99368ac5e9a79f60e8cb25
Milos Vasic <milos85vasic@gmail.com>
2018-06-08 14:04:48 +0200

    TensorFlow Examples: Connecting to TensorFlow - work in progress.

--- C++/Example_01/Hello_TensorFlow/tensorclient.cpp
+++ C++/Example_01/Hello_TensorFlow/tensorclient.cpp
@@ -1,10 +1,10 @@
-#include <QObject>
 #include <QDebug>
+#include <QObject>
 
 #include "tensorclient.h"
 
+//#import <tensorflow/core/public/session.h>
+
 TensorClient::TensorClient(QObject *parent) : QObject(parent) {}
 
-void TensorClient::tryTensorFlow() {
-    qDebug() << "Trying TensorFlow.";
-}
+void TensorClient::tryTensorFlow() { qDebug() << "Trying TensorFlow."; }
