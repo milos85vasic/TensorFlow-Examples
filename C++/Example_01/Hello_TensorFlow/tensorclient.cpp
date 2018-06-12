bb58ce41436dcccb7b374a1d025d171bc8727cfa
Milos Vasic <milos85vasic@gmail.com>
2018-06-12 16:25:58 +0200

    QML Examples: FIXME added.

--- C++/Example_01/Hello_TensorFlow/tensorclient.cpp
+++ C++/Example_01/Hello_TensorFlow/tensorclient.cpp
@@ -1,11 +1,13 @@
 #include <QDebug>
 #include <QObject>
 
-#include <tensorflow/cc/framework/ops.h>
 #include <tensorflow/cc/client/client_session.h>
+#include <tensorflow/cc/framework/ops.h>
 #include <tensorflow/core/framework/tensor.h>
 
 #include "tensorclient.h"
+
+// FIXME: .//third_party/eigen3/unsupported/Eigen/CXX11/Tensor !!!
 
 TensorClient::TensorClient(QObject *parent) : QObject(parent) {}
 
