0ae4c685c4368e2c9632d28b654426dcc3b0de5c
Milos Vasic <milos85vasic@gmail.com>
2018-06-20 11:43:06 +0200

    Running TensorFlow Session.

--- C++/Example_01/Hello_TensorFlow/tensorclient.cpp
+++ C++/Example_01/Hello_TensorFlow/tensorclient.cpp
@@ -2,7 +2,19 @@
 #include <QObject>
 
 #include "tensorclient.h"
+#include <tensorflow/core/platform/env.h>
+#include <tensorflow/core/public/session.h>
+
+using namespace tensorflow;
 
 TensorClient::TensorClient(QObject *parent) : QObject(parent) {}
 
-void TensorClient::tryTensorFlow() { qDebug() << "Trying TensorFlow."; }
+void TensorClient::tryTensorFlow() {
+    Session* session;
+    Status status = NewSession(SessionOptions(), &session);
+    if (!status.ok()) {
+        qDebug() << "Session failed.";
+    } else {
+        qDebug() << "Session successfully created.\n";
+    }
+}
